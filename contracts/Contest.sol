29c519511ac640384d21a4f5566c912066b2bd44
Sushant Barje <sushantbarje4@gmail.com>
2021-07-09 12:55:04 +0530

    smart contract add multiple contestants

--- contracts/Contest.sol
+++ contracts/Contest.sol
@@ -2,9 +2,40 @@ pragma solidity >=0.4.0;
 
 contract Contest {
 
-	string public contestant;
+	//Creating structure to model the contestant
+	struct Contestant {
+		uint id;
+		string name;
+		uint voteCount;
+	}
 
-	constructor () public{
-		contestant = "Tom";
+	constructor () public {
+		addContestant("Tom");
+		addContestant("Jerry");
+	}
+
+	/**
+	 * use mapping to get or fetch the contestant details
+	 * this is like 
+	 * contestant[1] => 1,tom,0
+	 * contestant[2] => 2,jerry,0
+	 * */
+	mapping(uint => Contestant) public contestants;
+
+	/**
+	* 	Public state variable to keep track of contestant count
+	* 	in other language we can find the length of contestants by len(contestants)
+	* 	but in blockchain this will return 
+	* 	contanstant[?] = "0,"",0
+	* 	So we have to keep track of contestants when the are added.
+	* */
+	uint public contestantsCount;
+
+	/**
+	 * Function to add Contestant
+	 * */
+	function addContestant (string memory _name) private {
+		contestantsCount++;
+		contestants[contestantsCount] = Contestant(contestantsCount, _name, 0);
 	}
 }
